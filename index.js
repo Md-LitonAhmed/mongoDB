3ed821e060480f23ff72667d3904e57080693f8b
“liton-ahmed” <litonahmed066@gmail.com>
2023-08-23 23:21:49 +0600

    CRUD Operations

--- index.js
+++ index.js
@@ -19,16 +19,7 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     const myCollection = client.db("School").collection("Students");
-    const myData = {
-      name: "Liton",
-      Roll: "101",
-      class: "12",
-      age: "19",
-      city: "Dhaka",
-    };
-    myCollection.insertOne(myData).then((result) => {
-      console.log("My Data added");
-    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -43,14 +34,14 @@ async function insert() {
   await client.connect();
   const myCollection2 = client.db("School").collection("Students");
   const myData = {
-    name: "Sayed",
-    Roll: "102",
-    class: "12",
-    age: "20",
-    city: "Barishal",
+    name: "Ayan",
+    Roll: "109",
+    class: "10",
+    age: "29",
+    city: "Rangpur",
   };
   myCollection2.insertOne(myData).then((result) => {
-    console.log("My Data 2 added");
+    console.log("My Data is added");
   });
 }
 // insert();
@@ -58,9 +49,98 @@ async function insert() {
 async function deleteData() {
   await client.connect();
   const myCollection2 = client.db("School").collection("Students");
-  const myDeleteData = { Roll: "102" };
-  myCollection2.deleteMany(myDeleteData).then((result) => {
-    console.log("One Data deleted where roll equal to 102");
+  const myDeleteData = { Roll: "101" };
+  myCollection2.deleteOne(myDeleteData).then((result) => {
+    console.log("One Data deleted where roll equal to 101");
   });
 }
-deleteData();
+// deleteData();
+
+async function deleteAll() {
+  await client.connect();
+  const myCollection = client.db("School").collection("Students");
+  myCollection.deleteMany().then((result) => {
+    console.log(result);
+  });
+}
+// deleteAll();
+
+function findData() {
+  const mydataBase = client.db("School");
+  const myCollection = mydataBase.collection("Students");
+  var FindObj = { Roll: "103" };
+  myCollection.findOne(FindObj).then((result) => {
+    console.log(result);
+  });
+}
+// findData();
+
+function FindAll() {
+  const myCollection = client.db("School").collection("Students");
+  myCollection
+    .find()
+    .toArray()
+    .then((result) => {
+      console.log(result);
+    });
+}
+// FindAll();
+
+function FindDataByProjection() {
+  const myCollection = client.db("School").collection("Students");
+  var itemObj = {};
+  var ItemProjection = { projection: { Roll: 1, name: 1 } };
+  myCollection
+    .find(itemObj, ItemProjection)
+    .toArray()
+    .then((result) => {
+      console.log(result);
+    });
+}
+// FindDataByProjection();
+
+// function FindDataByQuery() {
+//   const myCollection = client.db("School").collection("Students");
+//   var query = { city: "Dhaka", class: "12" };
+//   myCollection
+//     .find(query)
+//     .toArray()
+//     .then((result) => {
+//       console.log(result);
+//     });
+// }
+// FindDataByQuery();
+
+// function FindDataByLimit() {
+//   const myCollection = client.db("School").collection("Students");
+//   myCollection
+//     .find()
+//     .limit(3)
+//     .toArray()
+//     .then((result) => {
+//       console.log(result);
+//     });
+// }
+// FindDataByLimit();
+
+// function FindDataBySort() {
+//   const myCollection = client.db("School").collection("Students");
+//   myCollection
+//     .find()
+//     .sort({ Roll: 1 })
+//     .toArray()
+//     .then((result) => {
+//       console.log(result);
+//     });
+// }
+// FindDataBySort();
+
+function UpdateData() {
+  const myCollection = client.db("School").collection("Students");
+  var MyQuery = { Roll: "101" };
+  var setValue = { $set: { name: "Abu Sayed" } };
+  myCollection.updateOne(MyQuery, setValue).then((result) => {
+    console.log(result);
+  });
+}
+UpdateData();
